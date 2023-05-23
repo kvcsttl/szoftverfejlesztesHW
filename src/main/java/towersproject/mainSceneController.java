8d018afdddf8a0a61f13bb00563d1bfe0b65c464
Kovács Attila Ákos <koviati0123@gmail.com>
2023-05-23 20:58:59 +0200

    correcting the images in the startSceneController, adding info alert

--- src/main/java/towersproject/mainSceneController.java
+++ src/main/java/towersproject/mainSceneController.java
@@ -1,4 +1,18 @@
 package towersproject;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
 public class mainSceneController {
+
+    public void startSolution(ActionEvent actionEvent) {
+        Alert about = new Alert(Alert.AlertType.INFORMATION);
+        about.setTitle("Infó");
+        about.setHeaderText("Figyelem!");
+        about.setContentText("Mivel ez nem egy játék, a gomb megnyomása után\n a feladat magától fog megoldódni.");
+        about.showAndWait();
+    }
 }
