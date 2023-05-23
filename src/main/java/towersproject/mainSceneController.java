a9b93fc9f3a446904bd8b97efd935b66646e4067
Kovács Attila Ákos <koviati0123@gmail.com>
2023-05-23 21:15:01 +0200

    adding tinylog and exit button

--- src/main/java/towersproject/mainSceneController.java
+++ src/main/java/towersproject/mainSceneController.java
@@ -1,18 +1,25 @@
 package towersproject;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
+import org.tinylog.Logger;
 
 public class mainSceneController {
 
-    public void startSolution(ActionEvent actionEvent) {
+    @FXML
+    private void startSolution(ActionEvent actionEvent) {
         Alert about = new Alert(Alert.AlertType.INFORMATION);
         about.setTitle("Infó");
         about.setHeaderText("Figyelem!");
         about.setContentText("Mivel ez nem egy játék, a gomb megnyomása után\n a feladat magától fog megoldódni.");
         about.showAndWait();
+    }
+
+    @FXML
+    private void exitApp(ActionEvent actionEvent) {
+        Logger.info("Exiting");
+        Platform.exit();
     }
 }
