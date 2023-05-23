310495654863b5f965bf8d5da4e2a3ce5dbb4cbf
Kovács Attila Ákos <koviati0123@gmail.com>
2023-05-23 20:20:16 +0200

    modify the startSceneController

--- src/main/java/towersproject/startSceneController.java
+++ src/main/java/towersproject/startSceneController.java
@@ -1,4 +1,21 @@
 package towersproject;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class startSceneController {
+
+    @FXML
+    private void switchToMainScene(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/mainScene.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
