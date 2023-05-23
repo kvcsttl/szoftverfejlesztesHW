fa82dc46dc050a74674f0ec05f394ef3f0202a54
Kovács Attila Ákos <koviati0123@gmail.com>
2023-05-23 19:08:36 +0200

    Adding files

---
+++ src/main/java/towersproject/Main.java
@@ -0,0 +1,8 @@
+package towersproject;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println();
+    }
+}
---
+++ src/main/java/towersproject/TowersApplication.java
@@ -0,0 +1,19 @@
+package towersproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TowersApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/startScene.fxml"));
+        stage.setTitle("Szoftverfejlesztés beadandó");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+}
